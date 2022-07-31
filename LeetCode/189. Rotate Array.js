d3b032473ff7bd9222bf0288b80569a9207c450c
ksheer506@naver.com <ksheer506@naver.com>
2022-08-01 00:30:00 +0900

    폴더 구조 변경

---
+++ LeetCode/189. Rotate Array.js
@@ -0,0 +1,57 @@
+// https://leetcode.com/problems/rotate-array/description/
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+  const arr = nums.slice()
+  let temp = arr[0]
+  let i = 0;
+  let j = k % nums.length;
+    
+    while (j < arr.length) {
+      nums[j] = temp;
+      temp = arr[i+1];
+      i++;
+      j++;
+    }
+    
+    j = 0;
+    while (i < arr.length) {
+      nums[j] = arr[i];
+      i++;
+      j++;
+    }
+};
+
+const arr = [1,2]
+const k = 3
+
+rotate(arr, k)
+console.log(arr)
+/*
+temp = [nums[0]]: 초기값
+[1,2,3,4,5,6,7] k = 1
+     ^       ^   
+     j       i    
+[5,6,7,1,2,3,4]
+temp = [5]
+
+ nums[j] = temp.shift()
+ temp.push(nums[i+1])
+ i++, j++
+ 
+ while ( j < nums.length)까지 위 루프
+ j = 0으로 초기화 하고
+ while (i < nums.length) 까지 
+ nums[j] = nums[i]  
+ 
+[1,2,3,4] k = 2
+   ^   ^  
+   j   i  
+[3,4,1,2]
+temp = [4, undefined]
+
+[3,4,1,2]
+*/
