0a01b19c268ac1f56713e2528fac661983b8e4c1
ksheer506@naver.com <ksheer506@naver.com>
2022-08-04 02:19:53 +0900

    refactoring

--- 프로그래머스/5. 나누어 떨어지는 숫자 배열.js
+++ 프로그래머스/5. 나누어 떨어지는 숫자 배열.js
@@ -1,23 +1,22 @@
 function radixSort(arr) {
-  const radix = new Map([[0, []]]);
+  const radix = new Map();
   let sorted = arr;
   let current = 0;
 
   // 모든 수의 p번째 자릿수가 0이면 더이상 정렬할 필요 없음
-  while (radix.get(0).length < arr.length) {
-    for (let i = 0; i < 10; i++) {
-      radix.set(i, []);
-    }
+  while ((radix.get(0)?.length || 0) < arr.length) {
+    radix.clear();
     for (let j = 0; j < sorted.length; j++) {
       const digit = Math.floor(sorted[j] / (10 ** current)) % 10;
-      const column = radix.get(digit);
+      const column = radix.get(digit) || [];
 
       column.push(sorted[j]);
       radix.set(digit, column);
     }
     sorted = [];
     for (let k = 0; k < 10; k++) {
-      sorted = sorted.concat(radix.get(k));
+      // k의 값이 존재하면 sorted에 push()
+      radix.has(k) && sorted.push(...radix.get(k));
     }
     current++;
   }
