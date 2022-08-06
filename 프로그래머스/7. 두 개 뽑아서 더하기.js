9a731796afe0314444ead061c83bbe580b8a616d
ksheer506@naver.com <ksheer506@naver.com>
2022-08-06 23:59:06 +0900

    답안 수정

--- 프로그래머스/7. 두 개 뽑아서 더하기.js
+++ 프로그래머스/7. 두 개 뽑아서 더하기.js
@@ -4,7 +4,7 @@ function countingSort(arr) {
   const sorted = [];
   let i = 0;
   
-  while (i < freq.length) {
+  while (i < arr.length) {
     freq[arr[i]] ||= 0;
     freq[arr[i]] += 1;
     i++
@@ -25,7 +25,7 @@ function countingSort(arr) {
 
 function solution(numbers) {
   const sorted = countingSort(numbers);
-  // const sorted = numbers.sort((a, b) =>  a - b)
+  // ↑ numbers를 정렬하지 않아도 어차피 나중에 한 번 더 정렬해야 하므로 필요 없는 코드
   const answer = new Set();
     
   for (let i = 0; i < sorted.length; i++) {
@@ -37,12 +37,16 @@ function solution(numbers) {
     }
   }
     
-  return Array.from(answer);
+  return countingSort(Array.from(answer));
 }
 
 const numbers = [5,0,2,7];
 console.log(solution(numbers))
 // [0, 2, 5, 7]
-// [0, 2, 5, 7]
 // 2 5 7 7 9 12
-// i = 0 루프, j = i 루프
+// i = 0 루프, j = i+1 루프
+
+// [0, 2, 3, 7, 10]
+// [0, 2, 3, 7, 10]
+// 0 2 3 7 "10 5" ...
+// numbers를 정렬했어도 answer 배열은 정렬되지 않았을 수 있음
