e6b97e873bb3fd5c567b5a372bbf301c579816dd
ksheer506@naver.com <ksheer506@naver.com>
2022-08-06 20:54:49 +0900

    countingSort 작성

--- 프로그래머스/7. 두 개 뽑아서 더하기.js
+++ 프로그래머스/7. 두 개 뽑아서 더하기.js
@@ -1,28 +1,43 @@
 function countingSort(arr) {
-  const freq = new Array(arr.length + 1);
+  const max = Math.max(...arr);
+  const freq = new Array(max + 1);
+  const sorted = [];
+  let i = 0;
   
-  while (0) {
-    
+  while (i < freq.length) {
+    freq[arr[i]] ||= 0;
+    freq[arr[i]] += 1;
+    i++
+  }
+  
+  i = 0;
+  while (i < freq.length) {
+    if (freq[i]) {
+      freq[i] -= 1;
+      sorted.push(i);
+    } else {
+      i++;
+    }
   }
   
   return sorted;
-}
+};
 
 function solution(numbers) {
-    const sorted = numbers.sort((a, b) =>  a - b)
-    const answer = new Set();
+  const sorted = countingSort(numbers);
+  // const sorted = numbers.sort((a, b) =>  a - b)
+  const answer = new Set();
     
-    for (let i = 0; i < numbers.length; i++) {
-      for (let j = i + 1; j < numbers.length; j++) {
-        console.log(i, j)
-        const sum = numbers[i] + numbers[j];
-        if (!answer.has(sum)) {
-          answer.add(sum);
-        }
+  for (let i = 0; i < sorted.length; i++) {
+    for (let j = i + 1; j < sorted.length; j++) {
+      const sum = sorted[i] + sorted[j];
+      if (!answer.has(sum)) {
+        answer.add(sum);
       }
     }
+  }
     
-    return Array.from(answer);
+  return Array.from(answer);
 }
 
 const numbers = [5,0,2,7];
