d4bdfd3638ecf32c14d0190993b0594e6ae84647
ksheer506@naver.com <ksheer506@naver.com>
2022-08-22 00:41:56 +0900

    코드 수정

--- 프로그래머스/Lv. 1/018. 비밀지도.js
+++ 프로그래머스/Lv. 1/018. 비밀지도.js
@@ -9,7 +9,7 @@ function solution(n, arr1, arr2) {
   
   return res.map((e) => {
     const bin = e.toString(2);
-    return ('0'.repeat(n - bin.length) + bin).replace(/(0{1,}|1)/g, rep);
+    return ('0'.repeat(n - bin.length) + bin).replace(/\d/g, rep);
   })
 }
 
@@ -17,5 +17,5 @@ const n = 6;
 const arr1 = [46, 33, 33 ,22, 31, 50];
 const arr2 = [27 ,56, 19, 14, 14, 10];
 
-console.log(solution2(n, arr1, arr2))
-console.log(["######", "### #", "## ##", " #### ", " #####", "### # "])
+console.log(solution(n, arr1, arr2))
+console.log(["######", "###  #", "##  ##", " ####  ", "  #####", "### # "])
