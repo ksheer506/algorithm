eb679b3861ae0746233d6f4a22fb431f3ee951b9
ksheer506@naver.com <ksheer506@naver.com>
2022-10-23 20:48:09 +0900

    1차 풀이 완료

--- 프로그래머스/Lv. 2/021. 수식 최대화.js
+++ 프로그래머스/Lv. 2/021. 수식 최대화.js
@@ -1,17 +1,32 @@
 const CALCULATE = {
-  '+': arr => arr.reduce((a, c) => a + c, 0),
-  '-': arr => arr.reduce((a, c) => a - c, 0),
-  '*': arr => arr.reduce((a, c) => a * c, 1),
+  '+': (a, c) => Number(a) + Number(c),
+  '-': (a, c) => Number(a) - Number(c),
+  '*': (a, c) => Number(a) * Number(c),
+}
+
+function calculator(arr ,operator) {
+  let calc = [...arr];
+  let j = 1;
+  
+  while (j < calc.length) {
+    if (calc[j] !== operator) {
+      j += 2;
+      continue;
+    }
+  
+    const res = CALCULATE[operator](calc[j - 1], calc[j + 1]);
+    calc.splice(j - 1, 3, res);
+  }
+  
+  return calc;
 }
 
 function solution(expression) {
   // a. 숫자: 뒤에 연산자가 오는 연속 숫자, 맨 뒤에 오는 연속 숫자
   // b. 연산자
-  const splited = expression.match(/(\d{1,}($|(?=[^\d])))|[-\+\*]/g)
+  const splitedExp = expression.match(/(\d{1,}($|(?=[^\d])))|[-\+\*]/g)
   const operations = [...new Set(expression.match(/[-\+\*]/g))];
-  const pairNumberIdx = new Map();
   const priorities = [];
-  const results = [];
 
   // 1. 연산 우선순위 결정하기
   const permutation = (arr, selected) => {
@@ -28,24 +43,21 @@ function solution(expression) {
   }
 
   permutation([], new Set());
-  
-  // 2. 피연산자의 인덱스 찾기
-  for (let j = 0; j < splited.length; j++) {
-    const op = ['+', '-', '*'];
-    
-    if (op.includes(splited[j])) {
-      const prev = pairNumberIdx.get(splited[j]) || [];
-      
-      // 연산자의 양 옆 숫자의 인덱스를 추가
-      pairNumberIdx.set(splited[j], new Set([...prev, splited[j-1], splited[j+1]]));
-    }
-  }
-  
-  // 3. 우선순위에 따라 계산하기(절댓값)
 
-  // 3. results에서 최댓값 반환
+  // 2. 우선순위에 따라 계산하기(절댓값)
+  const results = priorities.map((p) => {
+    let calculation = splitedExp;
+    
+    for (let j = 0; j < p.length; j++) {
+      calculation = calculator(calculation, p[j])
+    }
+    
+    return Math.abs(calculation[0]);
+  });
+
+  // 4. results에서 최댓값 반환
   return Math.max(...results);
 }
 
-const expression = '100-200*300-500+20'
+const expression = "100-200*300-500+20"
 console.log(solution(expression));
