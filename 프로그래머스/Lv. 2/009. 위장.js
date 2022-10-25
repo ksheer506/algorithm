16806be3cac906fbad3fe38e761fc8f5abdf7462
ksheer506@naver.com <ksheer506@naver.com>
2022-10-26 02:03:18 +0900

    2차 풀이 완료: 시간 복잡도 개선

--- 프로그래머스/Lv. 2/009. 위장.js
+++ 프로그래머스/Lv. 2/009. 위장.js
@@ -9,25 +9,26 @@ function solution(clothes) {
   }
 
   const keys = [...closet.keys()];
-  const power = (depth, arr) => {
+  
+  const power = (depth, cases) => {
     if (depth >= keys.length) {
-      powerSet.push(arr);
+      powerSet.push(cases);
       return;
     }
-    
-    power(depth + 1, [...arr, keys[depth]]);
-    power(depth + 1, [...arr]);
+
+    // 멱집합을 만들지 않고 해당 멱집합에 대응되는 경우의 수를 배열에 저장
+    power(depth + 1, cases * closet.get(keys[depth]));
+    power(depth + 1, cases);
   };
   
-  power(0, []);
+  power(0, 1);
 
   for (let j = 0; j < powerSet.length; j++) {
-    if (!powerSet[j].length) continue;
-
-    answer += powerSet[j].reduce((a, c) => a * closet.get(c), 1);
+    answer += powerSet[j]
   }
-
-  return answer;
+  
+  // 멱집합에서 []도 1로 계산되는 것 제외
+  return answer - 1;
 };
 
 const clothes = [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]];
@@ -37,6 +38,7 @@ console.log(solution(clothes));
 1. N = 0부터 keys의 0번째 원소를 포함할지, 안 할지 분기
 - 포함하면 배열 arr에 원소 넣고 다음 재귀에 넘김
 - 포함 안하면 arr 그대로 다음 재귀에 넘김
+
 2. N = keys.length이면 이어온 arr을 결과 배열에 넣음
 */
 
@@ -59,3 +61,39 @@ const power = array => {
   return result;
 }
 */
+
+function nonRecursiveSolution(clothes) {
+  const closet = new Map();
+  let answer = 0;
+
+  for (let i = 0; i < clothes.length; i++) {
+    const prev = closet.get(clothes[i][1]) || 0;
+    closet.set(clothes[i][1], prev + 1);
+  }
+
+  const keys = [...closet.keys()];
+  const power = (arr) => {
+    const result = [0];
+
+    for (const value of arr) {
+      const length = result.length
+
+      for (let i = 0; i < length; i++) {
+        let prevCase = result[i] || 1;
+        
+        // 멱집합을 만들지 않고 해당 멱집합에 대응되는 경우의 수를 배열에 저장
+        result.push(prevCase * closet.get(value));
+      }
+    }
+
+    return result;
+  }
+  const powerSet = power(keys);
+
+  for (let j = 0; j < powerSet.length; j++) {
+    answer += powerSet[j]
+  }
+
+  // 멱집합에서 []도 1로 계산되는 것 제외
+  return answer - 1;
+};
