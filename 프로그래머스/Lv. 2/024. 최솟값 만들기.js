3be60bc221786429dd50febbae5f0b517ecc371a
ksheer506@naver.com <ksheer506@naver.com>
2022-10-29 11:23:28 +0900

    오류 수정

--- 프로그래머스/Lv. 2/024. 최솟값 만들기.js
+++ 프로그래머스/Lv. 2/024. 최솟값 만들기.js
@@ -2,7 +2,7 @@ function solution(A, B) {
   let answer = 0;
 
   A.sort((a, b) => a - b);
-  B.sort((a, b => b - a));
+  B.sort((a, b) => b - a);
 
   for (let i = 0; i < A.length; i++) {
     answer += A[i] * B[i];
