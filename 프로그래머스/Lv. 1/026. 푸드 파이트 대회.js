ff981d32a8339df0196504655aa24348dcd0f08d
ksheer506@naver.com <ksheer506@naver.com>
2022-11-05 20:21:16 +0900

    풀이 완료

---
+++ 프로그래머스/Lv. 1/026. 푸드 파이트 대회.js
@@ -0,0 +1,20 @@
+function solution(food) {
+  const dishes = [];
+  let player1 = '';
+  let player2 = '';
+  
+  for (let i = 1; i < food.length; i++) {
+    const half = Math.floor(food[i] / 2);
+    
+    dishes.push(`${i}`.repeat(half));
+  }
+  for (let j = 0; j < dishes.length; j++) {
+    player1 += dishes[j];
+    player2 = `${dishes[j]}${player2}`;
+  }
+
+  return `${player1}0${player2}`
+}
+
+const food = [1, 3, 4, 6];
+console.log(solution(food));
