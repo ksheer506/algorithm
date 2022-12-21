a0ea8687b7439a1c85a7ecee0c96772baba356db
ksheer506@naver.com <ksheer506@naver.com>
2022-12-21 13:43:37 +0900

    코드 수정

--- 프로그래머스/Lv. 3/001. 이중우선순위큐.js
+++ 프로그래머스/Lv. 3/001. 이중우선순위큐.js
@@ -5,7 +5,7 @@ const getParentIdx = (idx) => {
   return Math.floor(((idx || 1) - 1) / 2);
 }
 
-const swap = (idx1, idx2, heap) => {
+const swap = (heap, idx1, idx2) => {
   [heap[idx1], heap[idx2]] = [heap[idx2], heap[idx1]];
 }
 
@@ -32,34 +32,17 @@ const getValidLastIndex = (heap) => {
   return lastIdx;
 }
 
-const insert_maxHeap = (heap, item) => {
+const insertHeap = (heap, item, callback) => {
   heap.push(item); // 1. 힙의 마지막 위치에 노드 삽입
   let cIdx = heap.length - 1;
   let pIdx = getParentIdx(cIdx); // 2. 부모 노드의 인덱스 찾음
 
   // 3. 부모 노드, 새 노드의 값을 비교하면서 위치를 바꾸는 작업 반복
   while (pIdx < cIdx && pIdx >= 0) {
-    if (heap[cIdx] <= heap[pIdx]) break;
+    if (callback(heap[cIdx],heap[pIdx])) break;
 
     // 4. 부모 노드의 값이 작을 때 부모 노드와 새 노드의 위치를 바꿈
-    swap(cIdx, pIdx, heap);
-    cIdx = pIdx;
-    pIdx = getParentIdx(cIdx);
-  }
-
-  return heap;
-}
-const insert_minHeap = (heap, item) => {
-  heap.push(item); // 1. 힙의 마지막 위치에 노드 삽입
-  let cIdx = heap.length - 1;
-  let pIdx = getParentIdx(cIdx); // 2. 부모 노드의 인덱스 찾음
-
-  // 3. 부모 노드, 새 노드의 값을 비교하면서 위치를 바꾸는 작업 반복
-  while (pIdx < cIdx && pIdx >= 0) {
-    if (heap[cIdx] >= heap[pIdx]) break;
-
-    // 4. 부모 노드의 값이 클 때 부모 노드와 새 노드의 위치를 바꿈
-    swap(cIdx, pIdx, heap);
+    swap(heap, cIdx, pIdx);
     cIdx = pIdx;
     pIdx = getParentIdx(cIdx);
   }
@@ -76,10 +59,10 @@ const reArrange_maxHeap = (heap) => {
 
     if (heap[rIdx] >= max) break; // 2. 해당 노드가 두 자식 노드보다 크면 정렬 완료  
     if (heap[cIdx[0]] >= heap[cIdx[1]]) { // 3-1. 해당 노드와 가장 큰 자식 노드를 교체 
-      swap(rIdx, cIdx[0], heap);
+      swap(heap, rIdx, cIdx[0]);
       rIdx = cIdx[0]; // 3-2. 교체한 자식 노드의 인덱스로 해당 노드의 인덱스 재설정
     } else {
-      swap(rIdx, cIdx[1], heap);
+      swap(heap, rIdx, cIdx[1]);
       rIdx = cIdx[1];
     }
     cIdx = [2 * rIdx + 1, Math.min(2 * rIdx + 2, heap.length - 1)]; // 4. 다음 루프의 자식 노드의 인덱스 재설정
@@ -95,10 +78,10 @@ const reArrange_minHeap = (heap) => {
 
     if (heap[rIdx] <= min) break; // 2. 해당 노드가 두 자식 노드보다 작으면 정렬 완료  
     if (heap[cIdx[0]] <= heap[cIdx[1]]) { // 3-1. 해당 노드와 가장 작은 자식 노드를 교체 
-      swap(rIdx, cIdx[0], heap);
+      swap(heap, rIdx, cIdx[0]);
       rIdx = cIdx[0]; // 3-2. 교체한 자식 노드의 인덱스로 해당 노드의 인덱스 재설정
     } else {
-      swap(rIdx, cIdx[1], heap);
+      swap(heap, rIdx, cIdx[1]);
       rIdx = cIdx[1];
     }
     cIdx = [2 * rIdx + 1, Math.min(2 * rIdx + 2, heap.length - 1)]; // 4. 다음 루프의 자식 노드의 인덱스 재설정
@@ -107,22 +90,22 @@ const reArrange_minHeap = (heap) => {
 
 const extractMax = (heap) => {
   const lastI = getValidLastIndex(heap);
-  swap(0, lastI, heap); // 1. 루트 노드와 마지막 노드를 교체
-  const root = heap.pop(); // 2. 교체 전 루트 노드를 추출(최댓값) 
+  swap(heap, 0, lastI); // 1. 루트 노드와 마지막 노드를 교체
+  const max = heap.pop(); // 2. 교체 전 루트 노드를 추출(최댓값) 
 
   reArrange_maxHeap(heap) // 3. 최대 힙을 유지하도록 힙을 재정렬 
 
-  return root;
+  return max;
 }
 
 const extractMin = (heap) => {
   const lastI = getValidLastIndex(heap);
-  swap(0, lastI, heap); // 1. 루트 노드와 마지막 노드를 교체
-  const tail = heap.pop(); // 2. 교체 전 루트 노드를 추출(최솟값) 
+  swap(heap, 0, lastI); // 1. 루트 노드와 마지막 노드를 교체
+  const min = heap.pop(); // 2. 교체 전 루트 노드를 추출(최솟값) 
 
   reArrange_minHeap(heap) // 3. 최대 힙을 유지하도록 힙을 재정렬 
 
-  return tail;
+  return min;
 }
 
 const updateHeap = (heap) => {
@@ -137,7 +120,8 @@ const updateHeap = (heap) => {
 }
 
 function solution(operations) {
-  let synchronizeFlag = false;
+  // maxHeap, minHeap이 동일하지 않을 때, 두 Heap을 동기화를 하도록 플래그 설정
+  let pendingSync = false;
   const maxHeap = [];
   const minHeap = [];
 
@@ -145,18 +129,21 @@ function solution(operations) {
     const [op, strEl] = operations[i].split(' ');
     const e = Number(strEl);
 
-    if (synchronizeFlag) {
+    if (pendingSync) {
       updateHeap(maxHeap);
       updateHeap(minHeap);
       console.log('최신화', minHeap, maxHeap);
     }
     if (op === 'I') {
-      insert_maxHeap(maxHeap, e);
-      insert_minHeap(minHeap, e);
+      const maxCallback = (a, b) => a <= b
+      const minCallback = (a, b) => a >= b
+      
+      insertHeap(maxHeap, e, maxCallback);
+      insertHeap(minHeap, e, minCallback);
       
       countsMap.set(e, (countsMap.get(e) || 0) + 1);
       console.log('삽입', minHeap, maxHeap);
-      synchronizeFlag = false;
+      pendingSync = false;
       continue;
     }
     
@@ -172,7 +159,7 @@ function solution(operations) {
       countsMap.set(min, countsMap.get(min) - 1);
       extractMin(minHeap);
     }
-    synchronizeFlag = true;
+    pendingSync = true;
     console.log('제거', minHeap, maxHeap);
   }
 
@@ -182,7 +169,6 @@ function solution(operations) {
 }
 
 
-// const operations = ["I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"]
-const operations = ["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]
+const operations = ["I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"]
+//const operations = ["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]
 console.log(solution(operations));
-// console.log(solution(operations));
