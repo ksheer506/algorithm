957a05306515f52249dd72b87098fbeb31d1e939
김성현 <ksheer506@naver.com>
2024-08-06 21:52:49 +0900

    022. 큰 수 만들기 2차

--- 프로그래머스/Lv. 2/022. 큰 수 만들기.js
+++ 프로그래머스/Lv. 2/022. 큰 수 만들기.js
@@ -1,51 +1,43 @@
 function solution(number, k) {
-  let front = 0;
-  let i = 0;
-  let result = '';
-  let max = Math.max(...number.slice(0, k + 1));
-  const t0 = performance.now()
-  while (k > 0) {
-    const t1 = performance.now()
-    if (t1 - t0 > 1000) break;
-    
-    if (k >= number.length - front) {
-      result += '*'.repeat(k);
-      break;
-    }
-    if (number[i] !== `${max}`) {
-      result += '*';
-      i++;
-      continue;
-    }
-    
-    result += `${max}`;
-    k -= i - front;
-    front = i + 1;
-    i++;
-    console.log(result, front, k, max);
-    max = Math.max(...number.slice(front, front + k + 1));
-  }
-  result += number.slice(result.length);
+  let removed = 0
+  const L = number.length
+  const stack = [number[0]]
   
-  return result.replace(/\*/g, '');
+  for (let i = 1; i < L; i++) {
+    const current = stack.at(-1)
+    const candidate = number[i]
+
+    console.log({i, current, candidate, removed, stack: stack.join('')})
+    
+    if (removed >= k) {
+      stack.push(candidate)
+      continue
+    }
+    if (current < candidate) {
+      stack.pop()
+      stack.push(candidate)
+      removed += 1
+    } else if (current === candidate) {
+       stack.push(candidate)
+    } else {
+      removed += 1
+    }
+  }
+  
+  return stack.join('')
 }
 
-const number = '1119'
-const k = 3
+const number = '4177252841' // 실패 case
+const k = 4
+
 console.log(solution(number, k));
-console.log(Math.max(...'1'));
 
 /* 
-ex. 4177256841
-각각5722643156 번째로 큰 수
+1. 처음 숫자부터 시작해 다음 숫자와 비교
+2-1. 처음 >= 다음: 처음 숫자는 보관. 다음 숫자를 제거
+2-2. 처음 < 다음: 처음 숫자를 제거. 다음 숫자를 보관.
+3. 다음 숫자와 다다음 숫자에 다해 2 과정을 반복
 
-**77256841
-
-0. front = 0
-1. front ~ k까지 가장 큰 숫자의 인덱스 N을 확인
-2-1. N === k이면, number.slice(k) 반환
-2-2. N < k이면, number = number.slice(N),
-front = N + 1, k = k - N으로 1 작업 반복
-3. k <= 0이 되면 number 반환
+- 중간에 있는 숫자들을 제거할 수는 있지만 숫자들의 위치는 바꿀 수 없음 → 
 
 */
