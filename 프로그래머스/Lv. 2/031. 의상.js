111d55de0918e2641cd318307c4e35ed2d84777a
김성현 <ksheer506@naver.com>
2024-09-03 20:10:20 +0900

    031. 의상 풀이중(런타임 오류, 시간 초과)

---
+++ 프로그래머스/Lv. 2/031. 의상.js
@@ -0,0 +1,59 @@
+function solution(clothes) {
+  // 부위 → 옷 가짓수
+  const closet = new Map();
+  const powerSet = [];
+  
+  clothes.forEach(([_, type]) => {
+    closet.set(type, (closet.get(type) ?? 0) + 1);
+  })
+
+  const types = [...closet.keys()];
+
+  /* 1. 재귀함수 이용 */
+  const power = (index, cases) => {
+    if (index >= types.length) {
+      powerSet.push(cases);
+      return;
+    }
+
+    /* 종류 types[index]를 가짓수에 포함 O */
+    power(index + 1, cases * closet.get(types[index]));
+    /* 종류 types[index]를 가짓수에 포함 X */
+    power(index + 1, cases);
+  };
+
+  // power(0, 1);
+  
+  /* 2. bitwise 연산 이용 */
+  const L = types.length
+  const size = 1 << L
+  
+  for (let i = 0; i < size; i++) {
+    let cases = 1
+    
+    for (let j = 0; j < L; j++) {
+      if (i & (1 << j)) {
+        cases *= closet.get(types[j])
+      }
+    }
+    powerSet.push(cases)
+  }
+
+  // 멱집합에서 []도 1로 계산되는 것 제외
+  return powerSet.reduce((a, c) => a + c, -1);
+};
+
+const clothes = [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]
+const clothes2 = [["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]
+
+console.log(solution(clothes2))
+
+/*
+clothes 종류별로 분류했을 때 옷의 수 [2, 1, 3, 5, ...]
+1. 입을 옷의 종류는 1, 2, 3, ..., N(<=closet.size)가지가 가능하므로 종류별 옷의 가짓수 배열에 대한 멱집합을 구하는 것과 같음
+*/
+
+/*
+1. 재귀함수를 이용한 멱집합: 런타임 오류
+2. bitwise를 이용한 멱집합: 시간 초과
+*/
