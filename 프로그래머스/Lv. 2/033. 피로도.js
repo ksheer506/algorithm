f25292580899b92fd255caedc967423a47457323
김성현 <ksheer506@naver.com>
2024-09-10 19:17:53 +0900

    033. 피로도 순열을 이용한 풀이

--- 프로그래머스/Lv. 2/033. 피로도.js
+++ 프로그래머스/Lv. 2/033. 피로도.js
@@ -17,14 +17,30 @@ const permutation = (arr, L) => {
 }
 
 function solution(k, dungeons) {
-  var answer = -1;
-  return answer;
-}
+  const L = dungeons.length
+  const cases = permutation(dungeons, L)
+  let max = 0
 
+  for (let i = 0; i < cases.length; i++) {
+    const route = cases[i]
+    let health = k
+    let count = 0
+
+    for (let j = 0; j < L; j++) {
+      const [required, consumption] = route[j]
+      
+      if (health < required) {
+        break
+      }
+      health -= consumption
+      count += 1
+    }
+    max = Math.max(max, count)
+  }
+  return max
+}
 
 const k = 80
 const dungeons = [[80,20],[50,40],[30,10]]
 
 console.log(solution(k, dungeons))
-
-console.log(permutation([1, 2, 3, 4], 4).flat())
