d318299fbdd4a3e94e1d179c745b704643ac1550
ksheer506 <ksheer506@naver.com>
2025-11-17 09:19:00 +0900

    049. 석유 시추 풀이

--- 프로그래머스/Lv. 2/049. 석유 시추.js
+++ 프로그래머스/Lv. 2/049. 석유 시추.js
@@ -5,17 +5,16 @@ const DIRECTION = [
 function solution(land) {
   const W = land[0].length
   const D = land.length 
-  
   // 매장지 index(0부터) -> 매장량
-  const depositMap = {}
-  const deposit = []
+  const deposits = []
+  // 매장지 index에 대한 좌표 Set
+  const groupCoordinates = []
   let depositIndex = 0
+  const depositByColumn = []
   
-
-  
-  const getDeposit = (start) => {
+  const getDeposit = (startR, startC) => {
     const visited = new Set()
-    const needToVisit = [start]
+    const needToVisit = [[startR, startC]]
     let count = 0
     
     // 매장량이 없거나 land 밖인 경우 탐색 대상에 넣지 않음
@@ -44,21 +43,34 @@ function solution(land) {
     return [count, visited]
   }
   
-  const [d, v] = getDeposit([0, 3])
-  
-  console.log(d, [...v].map((c) => c.toString()))
-  
-  for (let r = 0; r < D; r++) {
-    for (let c = 0; c < W; c++) {
+  for (let c = 0; c < W; c++) {
+    let prevGroupIndex = -1
+    
+    for (let r = 0; r < D; r++) {
+      const pos = `${r},${c}`
+      const hasOil = land[r][c] === 1
+      const groupIndex = groupCoordinates.findIndex((c) => c.has(pos))
       
+      if (!hasOil) {
+        continue
+      }
+      // 매장 그룹의 최상단 위치에서만 해당 열의 매장량 기록
+      if (groupIndex === -1) {
+        const [deposit, visited] = getDeposit(r, c)
+        
+        groupCoordinates[groupIndex] = visited
+        depositByColumn[c] = (depositByColumn[c] ?? 0) + deposit
+        
+        groupIndex += 1
+      }
     }
   }
-  
+  return Math.max(...depositByColumn)
 }
 
 /**
  * (i, 0)가 출발점인 길찾기 문제와 유사함. 하지만 모든 0 <= i < land[0].length에 대해 계산하면 중복 계산이 존재하기 때문에 비효율적
- * -> 탐색한 좌표도 같이 반환해 
+ * -> 탐색한 좌표도 같이 반환해 중복 계산 피함
 */
 
 const land = [
