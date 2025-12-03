0f3ad972d00ee7b764b0e0b9154ae04ec3e589cc
ksheer506 <ksheer506@naver.com>
2025-12-04 08:55:12 +0900

    051. N개의 최소공배수 두번째 풀이

--- 프로그래머스/Lv. 2/051. N개의 최소공배수.js
+++ 프로그래머스/Lv. 2/051. N개의 최소공배수.js
@@ -14,6 +14,9 @@ function solution(arr) {
   return Object.entries(submultiple).reduce((a, [n, c]) => a * (n ** c), 1);
 }
 
+/**
+ * 어떤 수 `n`의 모든 약수를 구하는 함수
+*/
 const getSubmultiples = (n) => {
   const multiples = {}
   let isPrime = true
@@ -35,6 +38,25 @@ const getSubmultiples = (n) => {
   return isPrime ? { [n]: 1 } : multiples
 }
 
+/**
+ * [방법2] 두 수의 곱에서 최대공약수를 나누면 공약수가 1번만 포함된 숫자(최소공배수)를 얻을 수 있음 
+ * (a a b c) * (a b d) / (a b) = (a a b c d)
+ * -> 모든 숫자들을 곱해 해당 과정을 반복해나가면 중복된 약수들이 제거된 최대공배수를 구할 수 있음
+*/
+function solution2(num) {
+  return num.reduce((a, b) => a * b / gcd(a, b))
+}
+
+/**
+ * 유클리드 호제법: 두 정수의 최대공약수를 구하는 방법
+ * b > a 조건이 필요하지만 `arr`이 오름차순으로 제시되었기 때문에 정렬할 필요가 없음
+ */
+function gcd(a, b) {
+  return a % b ? gcd(b, a % b) : b
+}
+
 const arr = [1,2,3]
 
 console.log(solution(arr))
+
+console.log(gcd(4,18)) // 2 2, 2 3 3
