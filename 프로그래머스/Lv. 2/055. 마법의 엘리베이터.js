39b714e87a0f1590f9c3ce3b7ac167c6b51ea6b6
ksheer506 <ksheer506@naver.com>
2025-12-30 09:59:39 +0900

    055. 마법의 엘리베이터 DFS 풀이

--- 프로그래머스/Lv. 2/055. 마법의 엘리베이터.js
+++ 프로그래머스/Lv. 2/055. 마법의 엘리베이터.js
@@ -27,5 +27,39 @@ function solution(storey) {
  *   45층: -5,-40 / +5,-50
  */
 
+function solutionDFS(storey) {
+  const L = `${storey}`.length
+  let minCount = storey
+  
+  const getNthDigit = (n, digit) => {}
+  
+  const dfs = (current, digit, count = 0) => {
+    console.log({current, digit, count})
+    if (current === 0 || digit > L + 1) {
+      minCount = Math.min(count, minCount)
+      return
+    }
+    const num = current % (10 ** digit)
+    const downCount = 0
+    const upCount = 0
+    
+    // TODO: / 제거하기
+    dfs(current - num, digit + 1, count + num / (10 ** (digit - 1)))
+    dfs(current + (10 - num), digit + 1, count + (10 ** digit - num) / (10 ** (digit - 1)))
+  }
+  dfs(storey, 1)
+
+  return minCount
+}
+console.log("temp", 165 % 100)
+/**
+ * DFS를 이용한 방법
+ * N번째 자리에서 위로 갈지, 아래로 갈지 탐색
+ * ex. 1365
+ * 1. 1번째 자리(5)에 대해 아래로 가거나(1365 - 5), 위로 가거나(1365 + (10-5))
+ * 2. 1의 결과(1360, 1370)에서 두번째 자리에 대해 아래로 가거나(1360 - 60), 위로 가거나(1360 + (100-60))
+ */
+
 const storey = 165
 console.log(solution(storey))
+console.log(solutionDFS(storey))
