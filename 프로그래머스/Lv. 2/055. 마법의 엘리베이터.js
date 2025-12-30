f2b1980c1fd6d25a9896ababf0420eec2724f380
ksheer506 <ksheer506@naver.com>
2025-12-30 10:22:41 +0900

    055. 마법의 엘리베이터 DFS 풀이 완료

--- 프로그래머스/Lv. 2/055. 마법의 엘리베이터.js
+++ 프로그래머스/Lv. 2/055. 마법의 엘리베이터.js
@@ -18,11 +18,15 @@ function solution(storey) {
 }
 
 /**
- * 1. 특정 자릿수가 5이면 위로 가든 아래로 가든 동일함
+ * 1. 특정 자릿수가 4이하이면 아래로, 6이상이면 위로 올라가는 것이 최소
+ *   14층: -4,-10 / +6,-20
+ *   66층: +4,+30-100 / -6,-60
+ * 
+ * 2. 특정 자릿수가 5이면 위로 가든 아래로 가든 횟수는 동일함
  *   5층: -5
  *   55층: -5,-50 / +5,+40,-100
  * 
- * 2. 특정 자릿수가 5일 때 다음 자릿수가 4이하이면 아래로, 6이상이면 위로 올라가는 것이 최소
+ * 3. 하지만 1의 경우일 때 다음 자릿수가 4이하이면 아래로, 6이상이면 위로 올라가는 것이 최소
  *   65층: -5,+40,-100 / +5,+30,-100
  *   45층: -5,-40 / +5,-50
  */
@@ -31,35 +35,43 @@ function solutionDFS(storey) {
   const L = `${storey}`.length
   let minCount = storey
   
-  const getNthDigit = (n, digit) => {}
+  const getNthDigit = (n, digit) => {
+    const str = n.toString();
+    return Number(str[str.length - digit] ?? 0)
+  } 
   
+  /**
+   * @param current 현재 층 수
+   * @param digit 현재 탐색중인 자릿수
+   * @param count 이동 횟수
+   */
   const dfs = (current, digit, count = 0) => {
     console.log({current, digit, count})
-    if (current === 0 || digit > L + 1) {
+    if (current <= 0 || count > minCount) {
       minCount = Math.min(count, minCount)
       return
     }
-    const num = current % (10 ** digit)
-    const downCount = 0
-    const upCount = 0
-    
-    // TODO: / 제거하기
-    dfs(current - num, digit + 1, count + num / (10 ** (digit - 1)))
-    dfs(current + (10 - num), digit + 1, count + (10 ** digit - num) / (10 ** (digit - 1)))
+    const n = getNthDigit(current, digit)
+
+    dfs(current - n * (10 ** (digit - 1)), digit + 1, count + n)
+    dfs(current + (10 - n) * (10 ** (digit - 1)), digit + 1, count + (10 - n))
   }
   dfs(storey, 1)
 
   return minCount
 }
-console.log("temp", 165 % 100)
+
 /**
  * DFS를 이용한 방법
- * N번째 자리에서 위로 갈지, 아래로 갈지 탐색
+ * N번째 자리에서 위로 갈지, 아래로 갈지 탐색. 
+ * 첫번째 풀이와는 달리 위/아래 방향을 결정하지 않고 모두 탐색하기 때문에 효율성 면에서는 떨어짐
+ * 
  * ex. 1365
  * 1. 1번째 자리(5)에 대해 아래로 가거나(1365 - 5), 위로 가거나(1365 + (10-5))
  * 2. 1의 결과(1360, 1370)에서 두번째 자리에 대해 아래로 가거나(1360 - 60), 위로 가거나(1360 + (100-60))
  */
 
 const storey = 165
+
 console.log(solution(storey))
 console.log(solutionDFS(storey))
