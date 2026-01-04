6fc2487956a774855656f5ed5acd99ef8abae5db
ksheer506 <ksheer506@naver.com>
2026-01-04 22:04:39 +0900

    056. k진수에서 소수 개수 구하기 1차 풀이(시간 초과)

---
+++ 프로그래머스/Lv. 2/056. k진수에서 소수 개수 구하기.js
@@ -0,0 +1,28 @@
+function solution(n, k) {
+  const nums = n.toString(k).split("0")
+  
+  return n.toString(k).split("0").filter((n) => isPrime(Number(n))).length
+}
+
+const resolvedPrimes = new Set([2])
+
+const isPrime = (n) => {
+  if (n <= 1) {
+    return false
+  }
+  if (resolvedPrimes.has(n)) {
+    return true
+  }
+  for (let m = 2; m < n; m++) {
+    if (n % m === 0) {
+      return false
+    }
+  }
+  resolvedPrimes.add(n)
+  return true
+}
+
+const n = 110011 // 3
+const k = 10
+
+console.log(solution(n, k))
