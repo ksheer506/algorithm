a5145e06fffe26b0b2cdff9eaabb689551a5267a
ksheer506 <ksheer506@naver.com>
2026-01-29 17:43:46 +0900

    059. 숫자 변환하기 풀이 완료

---
+++ 프로그래머스/Lv. 2/059. 숫자 변환하기.js
@@ -0,0 +1,39 @@
+function solution(x, y, n) {
+  let minCount = Infinity;
+
+  const OPERATIONS = [
+    (a) => a + n,
+    (a) => a * 2,
+    (a) => a * 3,
+  ]
+
+  const dfs = (current, count) => {
+    if (current > y) {
+      return 
+    }
+    if (current === y) {
+      minCount = Math.min(count, minCount)
+      return 
+    }
+
+    for (op of OPERATIONS) {
+      dfs(op(current), count + 1)
+    }
+  }
+  dfs(x, 0)
+
+  return minCount < Infinity ? minCount : -1
+}
+
+/**
+ * 현재 숫자 x를 시작으로 세 개의 연산 op1(a + 5), op2(a * 2), op3(a *3)을 해보면서 y에 도달할 수 있는지 확인해야 함.
+ * 1. 매 순회마다 [op1, op2, op3]중 하나를 선택해 연산을 해야 하므로 DFS를 이용할 수 있음.
+ * 2. 매개변수: 연산 후 현재 값 current와 연산 횟수는 계속 기억하고 있어야 하므로 함수의 매개변수로 전달.
+ * 3. 탐색 중단 조건: current >= y. current === y라면 minCount와 count를 비교해서 최솟값으로 교체
+ */
+
+const x = 2
+const y = 5
+const n = 4
+
+console.log(solution(x, y, n)); // 2
